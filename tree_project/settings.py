ea42ebdb43296c7fe485c903f008ffc5f2e4a39a
TobeTek <katchyemma@gmail.com>
2021-06-24 05:18:54 +0100

    Init API deployment

--- tree_project/settings.py
+++ tree_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('45iu!--aby4(htk0tq%0z$a9jzt^5vs6f$w+_1rpn7147^qaat')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
